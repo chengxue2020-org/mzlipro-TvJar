166dd8ccd2fb659c76679d58327ffe790833c6ae
mzlipro <80456523+mzlipro@users.noreply.github.com>
2022-08-19 14:15:19 +0800

    Update QXBiubiu.java

--- app/src/main/java/com/github/catvod/spider/QXBiubiu.java
+++ app/src/main/java/com/github/catvod/spider/QXBiubiu.java
@@ -136,7 +136,7 @@ public class QXBiubiu extends Spider {
                     pic = Misc.fixUrl(webUrl, pic);
                     String link = subContent(jiequContent, getRuleVal("lianjieqian"), getRuleVal("lianjiehou")).get(0);
                     link = getRuleVal("ljqianzhui").isEmpty() ? (link + getRuleVal("ljhouzhui")) : ("x:" + getRuleVal("ljqianzhui")) + link + getRuleVal("ljhouzhui");
-                    String remark = 
+                    String remark = subContent(jiequContent, getRuleVal("tupianqian"), getRuleVal("tupianhou")).get(0);
                     if (!getRuleVal("gengxinqian").isEmpty() && !getRuleVal("gengxinhou").isEmpty()) {
                         try {
                             mark = subContent(jiequContent, getRuleVal("gengxinqian"), getRuleVal("gengxinhou")).get(0).replaceAll("\\s+", "").replaceAll("\\&[a-zA-Z]{1,10};", "").replaceAll("<[^>]*>", "").replaceAll("[(/>)<]", "");
