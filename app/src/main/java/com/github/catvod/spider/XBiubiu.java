7384d5cd354405a22d0eb7d8d60340b2ccca8c81
mzlipro <80456523+mzlipro@users.noreply.github.com>
2022-08-28 04:05:10 +0800

    Update XBiubiu.java

--- app/src/main/java/com/github/catvod/spider/XBiubiu.java
+++ app/src/main/java/com/github/catvod/spider/XBiubiu.java
@@ -313,11 +313,7 @@ public class XBiubiu extends Spider {
     public String playerContent(String flag, String id, List<String> vipFlags) {
         try {
             fetchRule();
-            String webUrl = getRuleVal("url") + id + getRuleVal("ua", Misc.UaWinChrome).trim();
-            if (ua.isEmpty())
-                ua = Misc.UaWinChrome;
-            headers.put("User-Agent", ua);
-            return headers;
+            String webUrl = getRuleVal("url") + id + fetch("ua");
             JSONObject result = new JSONObject();
             result.put("parse", 1);
             result.put("playUrl", "");
